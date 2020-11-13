85cbe9dbdbf0fe23ab8f3d0c2c96a1d202db2de0
minkimcello <minkimcello@gmail.com>
2020-11-13 15:15:00 -0500

    Typing attempt 1

--- packages/interactor/src/integration/cypress.ts
+++ packages/interactor/src/integration/cypress.ts
@@ -1,17 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { bigtestGlobals } from '@bigtest/globals';
+import { Interaction, ReadonlyInteraction } from '../interaction';
 
 declare const Cypress: any;
 declare const cy: any;
 
-function interact(interaction: any) {
+function interact(interaction: Interaction<void> | ReadonlyInteraction<void>) {
   return cy.document({ log: false }).then((doc: Document) => {
     bigtestGlobals.document = doc;
     return interaction;
   })
 }
 
-Cypress.Commands.add('do', (interaction: { description: any }) => {
+Cypress.Commands.add('do', (interaction: Interaction<void>) => {
   bigtestGlobals.runnerState = 'step';
   // tagging the log into a then will nest it underneath the interact thus resulting in "- step" as opposed to "step" but we can't turn the command into a promise to await the interact function.
   interact(interaction).then(()=> {
@@ -22,7 +23,7 @@ Cypress.Commands.add('do', (interaction: { description: any }) => {
   });  
 });
 
-Cypress.Commands.add('expect', (interaction: { description: any }) => {
+Cypress.Commands.add('expect', (interaction: ReadonlyInteraction<void>) => {
   bigtestGlobals.runnerState = 'assertion';
   interact(interaction).then(()=> {
     Cypress.log({
